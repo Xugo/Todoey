f69564c77421fb90c956a511613758e1c42f4c36
Xugo <KelvinNguyen@utexas.edu>
2018-02-16 14:58:11 -0600

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  Todoey
+//
+//  Created by Kelvin Nguyen on 2/16/18.
+//  Copyright © 2018 HowToNguyen. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+}
